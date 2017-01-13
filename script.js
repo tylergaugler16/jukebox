9fa13421f6d8d2a366192e9b4a3a885ab7eb138a
Tyler Gaugler <tylergaugler16@gmail.com>
2017-01-13 10:48:17 -0500

    stop timer from running while paused

--- script.js
+++ script.js
@@ -11,8 +11,12 @@ $(document).ready(function(){
   document.getElementById('pauseButton').addEventListener('click',function(){
     myJukeBox.pause();
   });
-  $jukebox.addEventListener('ended', function(){
+  $jukebox.addEventListener('ended', function(e){
+    console.log(e.target.src);
+    e.preventDefault();
     myJukeBox.next();
+    var songJustFinished = e.target.src;
+    myJukeBox.resetPlayer(songJustFinished);
   });
   document.getElementById('next').addEventListener('click',function(){
     myJukeBox.next();
@@ -33,13 +37,10 @@ $(document).ready(function(){
     }
   });
   $jukebox.addEventListener('playing', function(){
-    myJukeBox.displayTime();
+    var timer;
+    myJukeBox.displayTime(timer);
   });
-  $jukebox.addEventListener('ended',function(e){
-    e.preventDefault();
-    console.log("song ended");
-    myJukeBox.resetPlayer();
-  });
+
   document.getElementById('addToPlaylist').addEventListener('click', function(){
     var track = prompt('Name of the song you would like to add: ');
     myJukeBox.addToUserPlaylist(track);
@@ -66,8 +67,10 @@ function JukeBox(jukebox, songs, src){
   this.pause = function(){
     this.jukebox.pause();
     $('#songProgress').addClass('paused');
+    // checkIfPaused();
   };
   this.next = function(){
+    console.log("in next() function");
     var userPlaylist = this.userPlaylist;
     if(this.userPlaylist.length===0){
       if(this.src_index == songs.length-1){
@@ -75,17 +78,17 @@ function JukeBox(jukebox, songs, src){
         this.src = songs[this.src_index];
         this.jukebox.setAttribute('src',this.src);
         this.play();
+        startSongProgress();
       }
       else{
         this.src_index++;
         this.src = songs[this.src_index];
         this.jukebox.setAttribute('src',this.src);
         this.play();
+        startSongProgress();
       }
     }
     else{
-      console.log("TYLER");
-      console.log(this.userPlaylist.length);
       playUserPlaylist(this.userPlaylist);
     }
 
@@ -95,6 +98,7 @@ function JukeBox(jukebox, songs, src){
     this.src = songs[this.src_index];
     this.jukebox.setAttribute('src',this.src);
     this.play();
+    startSongProgress();
   };
   this.back = function(){
     if(this.src_index === 0){
@@ -107,8 +111,11 @@ function JukeBox(jukebox, songs, src){
     this.jukebox.setAttribute('src',this.src);
     this.play();
   };
-  this.displayTime = function(){
-     timer = setInterval(checkIfPaused, 1000);
+
+  this.displayTime = function(timer){
+     timer = setInterval(function(){
+       checkIfPaused(timer);
+     }, 1000);
   };
   this.addToUserPlaylist = function(track){
     var userPlaylist = this.userPlaylist;
@@ -126,21 +133,24 @@ function JukeBox(jukebox, songs, src){
         $('#userPlaylist').prepend("<li id=\'"+preview_url+"\'>"+name+"</li>");
 
         userPlaylist.push({url: preview_url, name: name});
-        console.log(userPlaylist.length);
+        // console.log(userPlaylist.length);
         if(jukebox.paused){
           playUserPlaylist(userPlaylist);
         }
       }
     });
   };
-  this.resetPlayer = function(){
-    console.log("resetting");
-    $('#songProgress').removeProp('right');
+  this.resetPlayer = function(songJustFinished){
+    $('#userPlaylist li').last().remove();
+    $('#songProgress').removeClass('paused');
+    // $('#songProgress').css('left','');
+    document.getElementById('songProgress').setAttribute('style','left:0px');
     $('#songProgress').animate({left: '0px'},'fast');
-    console.log("yup, i resetted");
+    // console.log("yup, i resetted");
+    // this.next();
   };
   playUserPlaylist = function(userPlaylist){
-    console.log("yeet");
+    // console.log("yeet");
     if(userPlaylist.length!==0){
       var nextSong = userPlaylist.pop();
       this.src = nextSong.url;
@@ -148,15 +158,10 @@ function JukeBox(jukebox, songs, src){
       this.jukebox.setAttribute('src',this.src);
       this.jukebox.play();
       startSongProgress();
-      setInterval(function(){
-        $('#userPlaylist li').last().remove();
-      },30000);
-
-      console.log(this.jukebox);
     }
   };
-  checkIfPaused = function(){
-    console.log(this.jukebox.paused);
+  checkIfPaused = function(timer){
+    // console.log("checking to see if paused");
     if(this.jukebox.paused){
       console.log("yeeet");
       clearInterval(timer);
@@ -164,8 +169,8 @@ function JukeBox(jukebox, songs, src){
     Totalseconds = Math.ceil(this.jukebox.currentTime);
     seconds = Totalseconds%60;
     minutes = Math.floor(Totalseconds/60);
-    console.log(minutes+":"+seconds);
-    console.log(Totalseconds);
+    // console.log(minutes+":"+seconds);
+    // console.log(Totalseconds);
     document.getElementById('currentTime').innerHTML = "0"+minutes+":"+seconds;
   };
   startSongProgress = function(){
@@ -181,7 +186,7 @@ function JukeBox(jukebox, songs, src){
 
     console.log(seconds);
     $('#songProgress').removeProp('left');
-    $('#songProgress').animate({right: '0px'}, seconds);
+    $('#songProgress').animate({left: '400px'}, seconds);
   };
 
 }
